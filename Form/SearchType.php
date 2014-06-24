35b0d74f7cf96ef29f4c37b81578815955cecab1
Scullwm <thomas@scullwm.com>
2014-06-24 11:32:16 +0000

    Add SearchForm basic

---
+++ Form/SearchType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Swm\VideotekBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class SearchType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('url')
+            ->add('title')
+            ->add('description')
+            ->add('tags')
+            ->add('statut')
+            ->add('hits')
+            ->add('fav');
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Swm\VideotekBundle\Entity\Video',
+        ));
+    }
+
+    public function getName()
+    {
+        return 'swm_videotekbundle_search';
+    }
+}
